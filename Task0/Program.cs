6a269e255f6f59d0f13adbbc39bd3ac9be97c105
Diana <dianadx24@gmail.com>
2023-07-13 17:10:07 +0300

    0

---
+++ Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.Write("Enter num : ");
+int num = int.Parse(Console.ReadLine());
+int firstdelit = num % 100;
+int second = firstdelit / 10;
+Console.WriteLine($"{second}");
