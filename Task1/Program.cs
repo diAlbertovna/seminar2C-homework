4eee53a0a97a211f132a2f3ea213cc39374965ec
Diana <dianadx24@gmail.com>
2023-07-13 17:10:24 +0300

    1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+//if(num > 1000)
+// int newthird = num / 10;
+    // Console.WriteLine($"{newthird}");
+
+Console.Write("Enter num : ");
+int num = int.Parse(Console.ReadLine());
+int third = num % 10; 
+if(num < 100) Console.WriteLine("third number not exist");
+if( num > 999)
+{
+     int newthird0 = num % 1000;
+     int newthird = newthird0 / 100;
+     Console.WriteLine($"{newthird}");
+}
+else
+{
+    Console.WriteLine($"{third}");
+}
